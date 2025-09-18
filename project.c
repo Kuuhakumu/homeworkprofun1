4dbdd1e5fb88f0b129e65959acb450f4742b104c
kuuhakumu <play.immin00@gmail.com>
2025-09-19 01:01:25 +0700

    added plan

--- project.c
+++ project.c
@@ -3,3 +3,26 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdbool.h>
+
+int add(){
+
+}
+int search(){
+
+}
+int update(){
+
+}
+int displaymenu(){
+
+}
+int delete_by_account(){
+
+}
+int list_all(){
+
+}
+
+int main(){
+    
+}
